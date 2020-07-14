f8b4c64537b66538572c83353de01569463e369d
HimanshuChoudahry <68221917+HimanshuChoudahry@users.noreply.github.com>
2020-07-14 09:27:29 +0530

    Add files via upload
    This is the Day1 Exercise1 solution.

---
+++ Exchange.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int variable1;
+            int variable2;
+            int exchange;
+
+            Console.Write("Enter the 1st variable : ");
+            variable1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the 2nd variable : ");
+            variable2 = Convert.ToInt32(Console.ReadLine());
+
+            exchange = variable1;
+            variable1 = variable2;
+            variable2 = exchange;
+
+            Console.WriteLine("The 1st variable : "+variable1);
+            Console.WriteLine("The 2nd variable : "+variable2);
+        }
+    }
+}
